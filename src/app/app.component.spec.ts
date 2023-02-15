244049694228558f7128a8b434d09a5431940faa
aliceyantonio <a2vallejolopez@gmail.com>
2023-02-15 21:31:11 +0100

    cambio nombre proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'boda'`, () => {
+  it(`should have as title '2024'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('boda');
+    expect(app.title).toEqual('2024');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('boda app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('2024 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'boda';
+  title = '2024';
 }
