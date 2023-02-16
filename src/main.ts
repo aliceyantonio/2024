eff8caf86875cf9b95b8467741c59b9f8bd2f27f
aliceyantonio <a2vallejolopez@gmail.com>
2023-02-16 17:46:21 +0100

    logica mostrado bot

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,10 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+if (!localStorage.getItem('currentUser')) {
+  document.getElementById('iDuck')!.style.display = 'none';
+} else {
+  document.getElementById('iDuck')!.style.display = 'initial';
+}
+
