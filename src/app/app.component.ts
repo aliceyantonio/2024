e8cf1436ac8ac1deaa0791085a14b54d1f979fe2
aliceyantonio <a2vallejolopez@gmail.com>
2023-02-16 18:23:51 +0100

    estilos generales login header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '2024';
+  isLogin: boolean = false;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      this.isLogin = location.href.includes('login');
+    })
+  }
 }
