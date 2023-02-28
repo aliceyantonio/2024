8006246334301ffd1be3e51942443a84019b3954
aliceyantonio <a2vallejolopez@gmail.com>
2023-02-28 18:32:25 +0100

    travelers conseguido

--- src/app/travelers/travelers.component.ts
+++ src/app/travelers/travelers.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-travelers',
@@ -6,6 +6,24 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./travelers.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class TravelersComponent {
+export class TravelersComponent implements OnInit, OnDestroy {
+
+  ngOnInit(): void {
+    document.getElementById('iDuck')!.style.display = 'none';
+    document.getElementById('header')!.classList.add('on-hover');
+    document.getElementById('footer')!.classList.add('on-hover');
+    document.querySelector('body')!.style.overflow = 'hidden';
+    document.querySelector('body')!.style.position = 'relative';
+    document.querySelector('html')!.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy(): void {
+    document.getElementById('iDuck')!.style.display = 'initial';
+    document.getElementById('header')!.classList.remove('on-hover');
+    document.getElementById('footer')!.classList.remove('on-hover');
+    document.querySelector('body')!.style.overflow = 'initial';
+    document.querySelector('body')!.style.position = 'initial';
+    document.querySelector('html')!.style.overflow = 'initial';
+  }
 
 }
