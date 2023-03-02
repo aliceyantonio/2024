1bc098d9ef7373766da66a9e1d7e93745d6a9240
aliceyantonio <a2vallejolopez@gmail.com>
2023-03-02 18:00:29 +0100

    mejora entrada en fullscreen

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,5 +9,21 @@ export class HeaderComponent {
 
   isMenuCollapsed = true;
 
+  fullScreen(){
+    if(document.fullscreen){
+      document.exitFullscreen();
+    } else{
+      document.querySelector('body')!.requestFullscreen()
+    }
+  }
 
+
+}
+
+declare global {
+  interface Document {
+    webkitIsFullScreen?: () => Promise<void>;
+    mozFullScreen ?: () => Promise<void>;
+    msFullscreenElement?: () => Promise<void>;
+  }
 }
