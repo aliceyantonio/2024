9a6a72bfa7c730c9a015281bea9cf3a8cea14bf0
aliceyantonio <a2vallejolopez@gmail.com>
2023-05-09 19:25:30 +0200

    mejoras en nosotros

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,7 +6,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class FormComponent {
 
--- src/app/us/us.component.ts
+++ src/app/us/us.component.ts
@@ -1,11 +1,13 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 // @ts-ignore
 import * as jsVectorMap from 'jsvectormap/dist/js/jsvectormap.js';
 import 'jsvectormap/dist/maps/world.js';
 import { Country } from '../models/country.model';
 import { CountriesService } from '../services/countries.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-us',
   templateUrl: './us.component.html',
@@ -13,13 +15,32 @@ import { CountriesService } from '../services/countries.service';
 })
 export class UsComponent implements OnInit {
 
+  @ViewChild('answersModal') answersModal: any;
+
+  modalRef!: NgbModalRef;
+  closeResult!: string;
+  answerShown: boolean = false;
+
+  usForm: FormGroup = this.fb.group({
+    question1: ['', Validators.required],
+    question2: ['', Validators.required],
+    question3: ['', Validators.required],
+    question4: ['', Validators.required],
+    question5: ['', Validators.required],
+  });
+  score = 0
+
   selectedCountries: Country[] = [];
   map: any;
+
+  visitedCountries = ["ES", "FR", "IT", "GB", "SE", "PT", "DE", "MA", "CO", "PA", "US", "PR", "MX", "PE", "BR", "IN", "QA", "CH", "BE", "NL", "CA", "BU", "CR"]
 
   constructor(
     private countriesService: CountriesService,
     private router: Router,
-    private location: Location
+    private location: Location,
+    private fb: FormBuilder,
+    public modal: NgbModal,
   ) { }
 
   ngOnInit(): void {
@@ -34,9 +55,12 @@ export class UsComponent implements OnInit {
       zoomStep: 1.2,
 
       onRegionSelected: (code: any, isSelected: any, selectedRegions: any) => {
-        this.router.navigate(['pais', code]).then(() => {
-          window.location.reload();
-        });
+
+        if (this.visitedCountries.includes(code)) {
+          this.router.navigate(['pais', code]).then(() => {
+            window.location.reload();
+          });
+        }
       },
 
       onRegionTooltipShow: (tooltip: any, code: any) => {
@@ -58,7 +82,10 @@ export class UsComponent implements OnInit {
         },
         hover: {
           fillOpacity: 0.5,
-        }
+        },
+        selected: {
+          fill: '#ffffff',
+        },
       },
 
       series: {
@@ -97,5 +124,41 @@ export class UsComponent implements OnInit {
         ]
       }
     });
+  }
+
+  checkForm() {
+    const correctAnswers = {
+      question1: 2,
+      question2: 3,
+      question3: 3,
+      question4: 1,
+      question5: 1,
+    }
+
+    if (this.usForm.value.question1 == correctAnswers.question1) { this.score++ }
+    if (this.usForm.value.question2 == correctAnswers.question2) { this.score++ }
+    if (this.usForm.value.question3 == correctAnswers.question3) { this.score++ }
+    if (this.usForm.value.question4 == correctAnswers.question4) { this.score++ }
+    if (this.usForm.value.question5 == correctAnswers.question5) { this.score++ }
+
+
+    if (!this.modal.hasOpenModals()) {
+      this.modalRef = this.modal.open(this.answersModal, { ariaLabelledBy: 'Eliminar-Proceso', size: 'l', centered: true, backdrop: 'static' });
+      this.modalRef.result.then((result) => {
+        this.closeResult = 'Closed with: ' + result;
+      });
+    }
+  }
+
+  showAnswers() {
+    this.usForm.patchValue({
+      question1: '2',
+      question2: '3',
+      question3: '3',
+      question4: '1',
+      question5: '1',
+    })
+    Array.from(document.getElementsByClassName('form-group')).forEach((element: any) => element.style.pointerEvents = 'none');
+    this.answerShown = true;
   }
 }
