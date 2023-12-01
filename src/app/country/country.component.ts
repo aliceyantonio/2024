e85b2d560a98c9915356c4da9bfe55ad98dc4214
aliceyantonio <a2vallejolopez@gmail.com>
2023-12-01 17:07:06 +0100

    eliminado mapa 2

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -9,29 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CountryComponent implements OnInit {
 
-  countryData: any;
-  countryPosts: any;
-  countryCode: string;
-  region: any;
-  capital: any;
-  currency: any;
-  population: any;
-  languages: any;
+  // countryData: any;
+  // countryPosts: any;
+  // countryCode: string;
+  // region: any;
+  // capital: any;
+  // currency: any;
+  // population: any;
+  // languages: any;
 
   constructor(
     private countriesService: CountriesService,
     private activatedRoute: ActivatedRoute,
   ) {
-    this.countryCode = this.activatedRoute.snapshot.paramMap.get('code')!;
+    // this.countryCode = this.activatedRoute.snapshot.paramMap.get('code')!;
 
-    this.countryData = this.countriesService.countriesDataEsp.find((country: any) => country['cca2'] == this.countryCode);
+    // this.countryData = this.countriesService.countriesDataEsp.find((country: any) => country['cca2'] == this.countryCode);
 
-    this.currency = Object.values(this.countryData['currencies'])[0];
-    this.languages = Object.values(this.countryData['languages']);
-    this.region = this.countryData.continent
-    this.capital = this.countryData.capital
+    // this.currency = Object.values(this.countryData['currencies'])[0];
+    // this.languages = Object.values(this.countryData['languages']);
+    // this.region = this.countryData.continent
+    // this.capital = this.countryData.capital
 
-    this.countryPosts = this.countriesService.countriesPosts.find(countryPost => countryPost.country == this.countryCode)?.posts;
+    // this.countryPosts = this.countriesService.countriesPosts.find(countryPost => countryPost.country == this.countryCode)?.posts;
   }
 
   ngOnInit(): void {
